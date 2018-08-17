a149075292d2cdc083e2a0510ffff08dc90571a9
osquitoOfTheNorth <oscar.menjivar@utoronto.ca>
2018-08-16 21:34:31 -0400

    more work

--- app/src/main/java/oscar/com/eater/Fragments/DateTimePickerDialogFragment.kt
+++ app/src/main/java/oscar/com/eater/Fragments/DateTimePickerDialogFragment.kt
@@ -15,6 +15,7 @@ import oscar.com.eater.R
 import oscar.com.eater.ViewModelFactories.ViewModelFactory
 import oscar.com.eater.ViewModels.DateTimePickerViewModel
 import oscar.com.eater.databinding.DateTimePickerDialogBinding
+import java.util.*
 
 class DateTimePickerDialogFragment : DialogFragment(), DateTimePickerClickListener{
 
@@ -36,14 +37,20 @@ class DateTimePickerDialogFragment : DialogFragment(), DateTimePickerClickListen
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val dataBinding = DataBindingUtil.inflate<DateTimePickerDialogBinding>(inflater,R.layout.date_time_picker_dialog,
                 container,false)
-        dataBinding.clickListener = viewModel
+        dataBinding.viewModel = viewModel
         return dataBinding.root
     }
 
     override fun onPickerButtonClicked(type: PickerType) {
+        val today = Calendar.getInstance()
+        val thisYear = today.get(Calendar.YEAR)
+        val thisMonth = today.get(Calendar.MONTH)
+        val thisDayOfMonth = today.get(Calendar.DAY_OF_MONTH)
+        val currentHour = today.get(Calendar.HOUR)
+        val currentMinute = today.get(Calendar.MINUTE)
         when(type){
-            PickerType.DATE -> { DatePickerDialog(context).show()}
-            PickerType.TIME -> { TimePickerDialog(context, null, 1,1,false).show()}
+            PickerType.DATE -> { DatePickerDialog(context, viewModel, thisYear,thisMonth,thisDayOfMonth).show()}
+            PickerType.TIME -> { TimePickerDialog(context, viewModel, currentHour,currentMinute,false).show()}
         }
     }
 }
--- app/src/main/java/oscar/com/eater/ViewModels/DateTimePickerViewModel.kt
+++ app/src/main/java/oscar/com/eater/ViewModels/DateTimePickerViewModel.kt
@@ -1,11 +1,24 @@
 package oscar.com.eater.ViewModels
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.arch.lifecycle.ViewModel
+import android.databinding.ObservableField
+import android.widget.DatePicker
+import android.widget.TimePicker
 import oscar.com.eater.Enum.PickerType
 import oscar.com.eater.Interfaces.DateTimePickerClickListener
+import java.text.SimpleDateFormat
+import java.util.*
 
-class DateTimePickerViewModel(private val listener : DateTimePickerClickListener) : ViewModel() {
+class DateTimePickerViewModel(private val listener : DateTimePickerClickListener) : ViewModel(),
+        DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
+    private var date : Calendar = Calendar.getInstance()
+    private val dateFormatter = SimpleDateFormat("EEE, MMM dd, yyyy")
+    private val timeFormatter = SimpleDateFormat("h:mm a")
+    val humanReadableDate : ObservableField<String> = ObservableField(dateFormatter.format(date.time))
+    val humanReadableTime : ObservableField<String> = ObservableField(timeFormatter.format(date.time))
     fun onSelectDateClicked(){
         listener.onPickerButtonClicked(PickerType.DATE)
     }
@@ -14,5 +27,19 @@ class DateTimePickerViewModel(private val listener : DateTimePickerClickListener
         listener.onPickerButtonClicked(PickerType.TIME)
     }
 
+    override fun onDateSet(p0: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        date.set(Calendar.YEAR,year)
+        date.set(Calendar.MONTH,month)
+        date.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+        humanReadableDate.set(dateFormatter.format(date.time))
+    }
 
+    override fun onTimeSet(p0: TimePicker?, hour: Int, minute: Int) {
+        date.set(date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DAY_OF_MONTH),hour,minute)
+        humanReadableTime.set(timeFormatter.format(date.time))
+    }
+
+    fun onSchedule(){
+
+    }
 }
